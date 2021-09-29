323e4be924fee705036599c628078ab137244eed
Emmanuel Siqueira <91622096+EmmaWok@users.noreply.github.com>
2021-09-29 14:04:05 -0300

    Exercicio

---
+++ Exercicio1.py
@@ -0,0 +1,9 @@
+print('Insira um numero abaixo:')
+num1 = int(input())
+print('Insira outro número abaixo')
+num2 = int(input())
+
+if num1 > num2:
+    print(f'{num1} é maior que {num2}.')
+else:
+    print(f'{num2} é maior que {num1}')
